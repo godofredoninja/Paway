dc0f8026068080cf903580dfe932b78a311515f8
GodoFredo <hell@godofredo.ninja>
2019-03-18 13:20:47 -0500

    update social media

--- src/scripts/app/app.social-media.js
+++ src/scripts/app/app.social-media.js
@@ -1,22 +1,18 @@
 export default links => {
   const urlRegexp = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \+\.-]*)*\/?$/; // eslint-disable-line
+  const templateSocialMedia = (name, url) => `<a href="${url}" title="Follow me in ${name}" target="_blank" rel="noopener noreferrer" class="godo-tracking"><i class="i-${name}"></i><span>${name}</span></a>`;
 
-  const box = document.querySelectorAll('.social-media');
-
-  [].forEach.call(box, el => {
-
+  function createPasteElement(parent) {
     Object.entries(links).forEach(([name, url]) => {
       if (typeof url === 'string' && urlRegexp.test(url)) {
-        const template = `<a href="${url}" title="Follow me in ${name}" target="_blank" rel="noopener noreferrer" class="godo-tracking">
-          <i class="i-${name}"></i>
-          <span>${name}</span>
-        </a>`;
+        const template = templateSocialMedia(name, url);
 
         const li = document.createElement('li');
         li.innerHTML = template;
-        el.appendChild(li);
+        parent.appendChild(li);
       }
     });
+  }
 
-  });
+  [].forEach.call(document.querySelectorAll('.social-media'), el => createPasteElement(el));
 };
