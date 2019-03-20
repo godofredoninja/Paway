8363ba9f821d0d2a013e8e54472c72181604bac9
GodoFredo <hell@godofredo.ninja>
2019-03-19 22:34:47 -0500

    add search

--- src/scripts/app/app.social-media.js
+++ src/scripts/app/app.social-media.js
@@ -1,6 +1,6 @@
 export default links => {
   const urlRegexp = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \+\.-]*)*\/?$/; // eslint-disable-line
-  const templateSocialMedia = (name, url) => `<a href="${url}" title="Follow me in ${name}" target="_blank" rel="noopener noreferrer" class="godo-tracking"><i class="i-${name}"></i><span>${name}</span></a>`;
+  const templateSocialMedia = (name, url) => `<a href="${url}" title="Follow me in ${name}" target="_blank" rel="noopener noreferrer"><i class="i-${name}"></i><span>${name}</span></a>`;
 
   function createPasteElement(parent) {
     Object.entries(links).forEach(([name, url]) => {
